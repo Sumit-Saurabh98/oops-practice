f846aa38dadc27b8918bc91594b2f6a8f7a62e64
Sumit-Saurabh98 <sumitsaurabh112@gmail.com>
2024-09-23 15:20:41 +0530

    done

---
+++ car.cpp
@@ -0,0 +1,36 @@
+/*
+Create a class named Car with the given specifications:
+
+Private Data Members:
+
+carID (int)
+brand (string)
+price (double)
+registrationCost (double)
+Public Member Functions:
+
+A constructor that initializes the carID, brand, and price of the Car and calculates the registration cost as 0.21x of the price.
+A destructor.
+A function display to print the details of the car including the calculated registration cost.
+You need to create an array of 3 Car objects, take input for each car from the command line, display the details of all cars.
+
+Example:
+
+Input:
+
+1
+CAR1
+32454.00
+2
+CAR2
+92554.00
+3
+CAR3
+62354.00
+
+Output:
+
+Car 1: Car ID: 1, Brand: Tesla, Price: 32454.00, Registration Cost: 6815.34
+Car 2: Car ID: 2, Brand: Audi, Price: 92554.00, Registration Cost: 19436.34
+Car 3: Car ID: 3, Brand: Toyota, Price: 62354.00, Registration Cost: 13094.34
+*/
---
+++ student.cpp
@@ -0,0 +1,40 @@
+/* 
+Create a class named Student with the following specifications:
+
+Private Data Members:
+studentID (int)
+name (string)
+marks (double)
+credits (double)
+Public Member Functions:
+A constructor that initializes the studentID, name, and marks of the students and calculates the credit as 0.12x of the marks.
+A destructor.
+A function display to print the details of the student including the calculated credits.
+You need to create an array of 3 Student objects, take input for each student from the command line, display the details of all students.
+
+Input:
+
+1
+John
+85
+2
+Alice
+97
+3
+Bob
+62
+
+Note:- print 2 digits after decimal
+in cpp you can use setprecision(2)
+in java you can use printf("%.2f", marks);
+
+Output:
+
+Student 1: Student ID: 201, Name: John, Marks: 85.00, Credits: 10.20
+Student 2: Student ID: 202, Name: Alice, Marks: 97.00, Credits: 11.64
+Student 3: Student ID: 203, Name: Bob, Marks: 62.00, Credits: 7.44
+*/
+
+#include <iostream>
+using namespace 
+
